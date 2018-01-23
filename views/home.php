94715d8290adc6cfdce4cefbfe57a11fdf56fc76
Ricardo de Oliveira <ricardo.reksystem@gmail.com>
2018-01-23 10:23:11 -0200

    Criando estrutura MVC

---
+++ views/home.php
@@ -0,0 +1,8 @@
+<h1>Clientes</h1>
+<?php
+    foreach($posts as $post) 
+    {    
+        echo "<h3>{$post['primeiro_nome']} {$post['ultimo_nome']}</h3>";
+        echo $post['email']." <hr/>";
+    }
+?>
